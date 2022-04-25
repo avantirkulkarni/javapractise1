55d664f36d02f8993aafb4a64f59b87157b26b35
Sumit Kulkarni <kulkarni.sumit.r@gmail.com>
2022-04-25 14:21:49 +0530

    Addition

---
+++ src/javapractice/Addition.java
@@ -0,0 +1,13 @@
+package javapractice;
+
+public class Addition {
+	
+	public static void main(String args[])
+	{
+		int x=10;
+		int y=20;
+		int z=x+y;
+		System.out.println("Addition of x and y" +z);
+	}
+
+}
